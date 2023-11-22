6d04c97fe41572b50281ce3a0821c2f11ca667cd
Toan <nguyentoan0275@gmail.com>
2023-11-22 13:01:07 -0500

    created working test of readme generator

--- index.js
+++ index.js
@@ -1,17 +1,100 @@
 // TODO: Include packages needed for this application
-const generateMarkdown = require('./utils/generateMarkdown')
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//starter code ?
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
 
-let response = {
-    title: 'Test'
-}
-console.log(generateMarkdown(response))
 // Function call to initialize app
 init();
+
+var inquirer = require("inquirer");
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    message: "What is your project title?",
+    name: "title",
+    default: "",
+  },
+  {
+    type: "input",
+    message: "Describe your project?",
+    name: "description",
+    default: "",
+  },
+  {
+    type: "input",
+    message: "What are the installation steps?",
+    name: "install",
+    default: "",
+  },
+  {
+    type: "input",
+    message: "What is your project for?",
+    name: "usage",
+    default: "",
+  },
+  {
+    type: "input",
+    message: "Were there any contributions to your project?",
+    name: "contribution",
+    default: "",
+  },
+  {
+    type: "input",
+    message: "What are the instructions for testing?",
+    name: "testing",
+    default: "",
+  },
+  {
+    type: "input",
+    message: "What is your Github username?",
+    name: "username",
+    default: "",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+    default: "",
+  },
+  {
+    type: "list",
+    message: "Choose a license for your project?",
+    name: "license",
+    choices: ["MIT", "GPL", "WTFPL", "ISC"],
+    default: "",
+  },
+];
+inquirer
+  .prompt(questions)
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    let response = {
+      title: answers.title,
+      description: answers.description,
+      install: answers.install,
+      usage: answers.usage,
+      contribution: answers.contribution,
+      testing: answers.testing,
+      username: answers.username,
+      email: answers.email,
+      license: answers.license,
+    };
+    let responseMD = generateMarkdown(response);
+    // TODO: Create a function to write README file
+    fs.writeFile("test.md", responseMD, (err) => {
+      if (err) throw err;
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,102 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+  switch (data.license) {
+    case "MIT":
+      return `(https://img.shields.io/badge/License-MIT-yellow.svg)]`;
+      break;
+    case "GPL":
+      return `(https://img.shields.io/badge/License-GPLv3-blue.svg)])`;
+      break;
+    case "WTFPL":
+      return `(https://img.shields.io/badge/License-WTFPL-brightgreen.svg)]`;
+      break;
+    case "ISC":
+      return `(https://img.shields.io/badge/License-ISC-blue.svg)]`;
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data) {
+  switch (data.license) {
+    case "MIT":
+      return `(https://opensource.org/licenses/MIT)`;
+      break;
+    case "GPL":
+      return `(https://www.gnu.org/licenses/gpl-3.0)`;
+      break;
+    case "WTFPL":
+      return `(http://www.wtfpl.net/about/)`;
+      break;
+    case "ISC":
+      return `(https://opensource.org/licenses/ISC)`;
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {
+  switch (data.license) {
+    case "MIT":
+      return `[![License: MIT]`;
+      break;
+    case "GPL":
+      return `[![License: GPL v3]`;
+      break;
+    case "WTFPL":
+      return `[![License: WTFPL]`;
+      break;
+    case "ISC":
+      return `[![License: ISC]`;
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  // renderLicenseBadge()
+  // renderLicenseLink()
+  // renderLicenseSection()
   return `# ${data.title}
 
+## Table of Contents
+-[Installation](#installation)<br>
+-[Usage](#usage)<br>
+-[Contributing](#contributing)<br>
+-[Tests](#tests)<br>
+-[Questions](#questions)<br>
+-[License](#license)<br>
+
+## Description
+${data.description}
+
+## Installation
+${data.install}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.testing}
+
+## Questions
+What is my github repository?<br>
+https://github.com/${data.username}
+
+What is my email and how can you reach me?<br>
+My email is ${data.email}, please feel free to send me an email with any questions regarding projects or colllaborations
+
+## License
+${data.license}
 `;
 }
 
